4975a55005eb4e8db6e9a9abd504499d40d15ae4
Chaozz <hzzhouchao89@gmail.com>
2017-03-24 03:05:46 -0400

    edit doc

--- ormini/utils.py
+++ ormini/utils.py
@@ -1,32 +1,5 @@
 class Dict(dict):
-    '''
-    Simple dict but support access as x.y style.
-    >>> d1 = Dict()
-    >>> d1['x'] = 100
-    >>> d1.x
-    100
-    >>> d1.y = 200
-    >>> d1['y']
-    200
-    >>> d2 = Dict(a=1, b=2, c='3')
-    >>> d2.c
-    '3'
-    >>> d2['empty']
-    Traceback (most recent call last):
-        ...
-    KeyError: 'empty'
-    >>> d2.empty
-    Traceback (most recent call last):
-        ...
-    AttributeError: 'Dict' object has no attribute 'empty'
-    >>> d3 = Dict(('a', 'b', 'c'), (1, 2, 3))
-    >>> d3.a
-    1
-    >>> d3.b
-    2
-    >>> d3.c
-    3
-    '''
+    """dict with d.x feature"""
 
     def __init__(self, keys=(), values=(), **kw):
         super(Dict, self).__init__(**kw)
